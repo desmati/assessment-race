4fb5c528160711507f9725d1890f7ce4a59f347d
Ila Kohan <ilakohansal@gmail.com>
2023-06-06 18:51:09 +0200

    add-button

--- lose/script.js
+++ lose/script.js
@@ -1,3 +1,7 @@
+function redirectToGame() {
+  window.location.href = "../game/index.html";
+}
+
 window.addEventListener("load", function () {
   const canvas = document.getElementById("canvas1");
   const ctx = canvas.getContext("2d");
